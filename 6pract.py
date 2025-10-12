6d5d69959b95a7e8f4694fd5621226699667a213
im0zero <scottyfo0579@gmail.com>
2025-10-12 19:58:23 -0400

    Update 6pract.py

--- 6pract.py
+++ 6pract.py
@@ -1,85 +1,120 @@
 # Задание 1
-n = int(input('Введите длину массива: '))
-m = int(input('Введите число M: '))
+n = int(input('Сколько чисел: '))
+m = int(input('Число M: '))
 x = []
 y = []
 
-for i in range(n):
-    element = int(input(f'Введите {i} элемент: '))
-    x.append(element)
+i = 0
+while i < n:
+    num = int(input(f'Число {i}: '))
+    x.append(num)
+    i += 1
 
-for element in x:
-    if abs(element) > m:
-        y.append(element)
+i = 0
+while i < n:
+    if abs(x[i]) > m:
+        y.append(x[i])
+    i += 1
 
-print('Введённое число M:', m)
-print('Массив X:', x)
-print('Массив Y:', y)
+print('M =', m)
+print('X =', x)
+print('Y =', y)
 
 # Задание 2
-n = int(input('Введите длину массива: '))
+n = int(input('Сколько чисел: '))
 a = []
 
-for i in range(n):
-    element = int(input(f'Введите {i} элемент: '))
-    a.append(element)
+i = 0
+while i < n:
+    num = int(input(f'Число {i}: '))
+    a.append(num)
+    i += 1
 
-print('Исходный массив:', a)
+print('Было:', a)
 
-for i in range(len(a)):
+i = 0
+while i < n:
     if a[i] < 0:
         a[i] = -a[i]
+    i += 1
 
-print('Полученный массив:', a)
+print('Стало:', a)
 
 # Задание 3
-n = int(input('Введите количество элементов массива: '))
+n = int(input('Сколько чисел: '))
 arr = []
 
-for i in range(n):
-    element = int(input(f'Введите {i} элемент: '))
-    arr.append(element)
+i = 0
+while i < n:
+    num = int(input(f'Число {i}: '))
+    arr.append(num)
+    i += 1
 
-max_element = max(arr)
-print('Максимальный элемент:', max_element)
-print('Массив в обратном порядке:', arr[::-1])
+max_num = arr[0]
+i = 1
+while i < n:
+    if arr[i] > max_num:
+        max_num = arr[i]
+    i += 1
+
+print('Самое большое:', max_num)
+print('Наоборот:', arr[::-1])
 
 # Задание 4
-n = int(input('Введите количество элементов массива: '))
+n = int(input('Сколько чисел: '))
 arr = []
 
-for i in range(n):
-    element = int(input(f'Введите {i} элемент: '))
-    arr.append(element)
+i = 0
+while i < n:
+    num = int(input(f'Число {i}: '))
+    arr.append(num)
+    i += 1
 
-min_element = min(arr)
-min_index = arr.index(min_element)
-print('Минимальный элемент:', min_element)
-print('Индекс минимального элемента:', min_index)
+min_num = arr[0]
+min_index = 0
+i = 1
+while i < n:
+    if arr[i] < min_num:
+        min_num = arr[i]
+        min_index = i
+    i += 1
+
+print('Самое маленькое:', min_num)
+print('Его номер:', min_index)
 
 # Задание 5
-n = 8
 d = []
-for i in range(n):
-    element = int(input(f'Введите {i} элемент массива D: '))
-    d.append(element)
+i = 0
+while i < 8:
+    num = int(input(f'Число {i}: '))
+    d.append(num)
+    i += 1
 
-sum_odd = 0
-for i in range(len(d)):
-    if i % 2 == 1:
-        sum_odd += d[i]
+s = 0
+i = 1
+while i < 8:
+    s += d[i]
+    i += 2
 
 print('Массив D:', d)
-print('Сумма элементов с нечетными индексами:', sum_odd)
+print('Сумма нечетных:', s)
 
 # Задание 9
-n = int(input('Введите количество элементов массива: '))
+n = int(input('Сколько чисел: '))
 arr = []
 
-for i in range(n):
-    element = float(input(f'Введите {i} элемент: '))
-    arr.append(element)
+i = 0
+while i < n:
+    num = float(input(f'Число {i}: '))
+    arr.append(num)
+    i += 1
 
-min_abs_element = min(arr, key=abs)
-print('Минимальный по модулю элемент:', min_abs_element)
-print('Массив в обратном порядке:', arr[::-1])
+min_abs = abs(arr[0])
+i = 1
+while i < n:
+    if abs(arr[i]) < min_abs:
+        min_abs = abs(arr[i])
+    i += 1
+
+print('Самое маленькое по модулю:', min_abs)
+print('Наоборот:', arr[::-1])
