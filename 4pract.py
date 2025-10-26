218b39bc2d1c35cd6693cb65cd0969f46c121242
im0zero <scottyfo0579@gmail.com>
2025-10-26 21:39:30 +0300

    Update 4pract.py

--- 4pract.py
+++ 4pract.py
@@ -1,129 +1,44 @@
-# Задача 1
-a = int(input("Введите A: "))
-b = int(input("Введите B: "))
-
-print("Числа:")
-i = a
-while i <= b:
-    print(i)
-    i += 1
-
-# Задача 2
-a = int(input("Введите A: "))
-b = int(input("Введите B: "))
-
-print("Числа:")
-if a < b:
-    i = a
-    while i <= b:
-        print(i)
-        i += 1
-else:
-    i = a
-    while i >= b:
-        print(i)
-        i -= 1
-
-# Задача 3
-a = int(input("Введите A: "))
-b = int(input("Введите B: "))
-
-print("Нечетные числа:")
-i = a
-while i >= b:
+#1
+A = 10
+B = 2
+for num in range(A,B):
+    num += 1
+    print(num)
+#2
+A = int(input("Введите A: "))
+B = int(input("Введите B: "))
+if A > B:
+    for num in range(B,A+1):
+        print(num)
+if A < B:
+    for num in range(B,A-1,-1):
+        print(num)
+#3
+A = int(input("Введите A: "))
+B = int(input("Введите B: "))
+i = A
+while i >= B:
     if i % 2 != 0:
         print(i)
     i -= 1
-
-# Задача 4
-n = int(input("Сколько чисел: "))
-s = 0
+#4
+n = int(input("Введите количество: "))
+sum = 0
 count = 0
 
 while count < n:
     num = int(input("Введите число: "))
-    s += num
+    sum += num
     count += 1
 
-print("Сумма:", s)
-
-# Задача 5
-n = int(input("Введите n: "))
-s = 0
-i = 1
-
-while i <= n:
-    s += i * i * i
-    i += 1
-
-print("Сумма кубов:", s)
-
-# Задача 6
-n = int(input("Введите n: "))
-f = 1
-i = 1
-
-while i <= n:
-    f = f * i
-    i += 1
-
-print("Факториал:", f)
-
-# Задача 7
-n = int(input("Введите n: "))
-sum_f = 0
-f = 1
-i = 1
-
-while i <= n:
-    f = f * i
-    sum_f += f
-    i += 1
-
-print("Сумма факториалов:", sum_f)
-
-# Задача 8
-n = int(input("Введите n: "))
-i = 1
-
-while i <= n:
-    j = 1
-    while j <= i:
-        print(j, end="")
-        j += 1
-    print()
-    i += 1
-
-# Задача 9
-n = int(input("Сколько чисел Фибоначчи: "))
-a = 0
-b = 1
-s = 0
-count = 0
-
-while count < n:
-    s += a
-    a, b = b, a + b
-    count += 1
-
-print("Сумма:", s)
-
-# Задача 10
-n = int(input("Сколько чисел: "))
-k = int(input("С какого номера: "))
-a = 0
-b = 1
-s = 0
-count = 0
-current = 1
-
-while current < k:
-    a, b = b, a + b
-    current += 1
-
-while count < n:
-    s += a
-    a, b = b, a + b
-    count += 1
-
-print("Сумма:", s)
+print(f'Сумма чисел: {sum}')
+print(f'Кол-во чисел: {count}')
+#5
+n = int(input('введите число: '))
+f = 0
+summ = 0
+while n > 0:
+    f += 1
+    summ += f**3
+    n -=1
+print(f'Сумма равна {summ}')
